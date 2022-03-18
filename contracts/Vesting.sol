e77d48ce7c4e9c65ab2cd19f02fecf3fa968ec3a
jonekell1002 <jonekell1002@gmail.com>
2022-03-18 08:37:57 +0100

    initial project

---
+++ contracts/Vesting.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract LinearVesting is Ownable, ReentrancyGuard {
+
+}
